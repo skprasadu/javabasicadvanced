20c78da0d973b43b44a77ab4337cf4b8b4c69ebc
juancastorena <03juan.c.castorena@gmail.com>
2022-02-16 21:20:30 -0600

    immutable fixes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-## Exceptions
+## Immutable
+
--- src/main/java/com/hcl/javabasicadvanced/immutable/ImmutableDemo.java
+++ src/main/java/com/hcl/javabasicadvanced/immutable/ImmutableDemo.java
@@ -3,9 +3,9 @@ package com.hcl.javabasicadvanced.immutable;
 import com.hcl.javabasicadvanced.model.Employee;
 
 public class ImmutableDemo {
-	public static void main(String ar[]) {
-		Employee e = new Employee("ABC123");
-		String s1 = e.getPancardNumber();
-		System.out.println("Pancard Number: " + s1);
-	}
+    public static void main(String ar[]) {
+        Employee e = new Employee("ABC123");
+        String s1 = e.getPancardNumber();
+        System.out.println("Pancard Number: " + s1);
+    }
 }
