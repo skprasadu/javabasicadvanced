b7b783554361768895845c9585687c2bb8cefd0a
juancastorena <03juan.c.castorena@gmail.com>
2022-02-17 12:54:13 -0600

    Fixes

--- src/main/java/com/hcl/javabasicadvanced/collection/TestStringComparisonBasicDotEquals.java
+++ src/main/java/com/hcl/javabasicadvanced/collection/TestStringComparisonBasicDotEquals.java
@@ -1,12 +1,13 @@
 package com.hcl.javabasicadvanced.collection;
+
 class TestStringComparisonBasicDotEquals {
-	public static void main(String args[]) {
-		String s1 = "Sachin";
-		String s2 = "Sachin";
-		String s3 = new String("Sachin");
-		String s4 = "Saurav";
-		System.out.println(s1.equals(s2));// true
-		System.out.println(s1.equals(s3));// true
-		System.out.println(s1.equals(s4));// false
-	}
+    public static void main(String args[]) {
+        String s1 = "Sachin";
+        String s2 = "Sachin";
+        String s3 = new String("Sachin");
+        String s4 = "Saurav";
+        System.out.println(s1.equals(s2));// true
+        System.out.println(s1.equals(s3));// true
+        System.out.println(s1.equals(s4));// false
+    }
 }
--- src/main/java/com/hcl/javabasicadvanced/collection/TestStringComparisonCompareTo.java
+++ src/main/java/com/hcl/javabasicadvanced/collection/TestStringComparisonCompareTo.java
@@ -1,11 +1,12 @@
 package com.hcl.javabasicadvanced.collection;
+
 class TestStringComparisonCompareTo {
-	public static void main(String args[]) {
-		String s1 = "Sachin";
-		String s2 = "Sachin";
-		String s3 = "Ratan";
-		System.out.println(s1.compareTo(s2));// 0
-		System.out.println(s1.compareTo(s3));// 1(because s1>s3)
-		System.out.println(s3.compareTo(s1));// -1(because s3 < s1 )
-	}
+    public static void main(String args[]) {
+        String s1 = "Sachin";
+        String s2 = "Sachin";
+        String s3 = "Ratan";
+        System.out.println(s1.compareTo(s2));// 0
+        System.out.println(s1.compareTo(s3));// 1(because s1>s3)
+        System.out.println(s3.compareTo(s1));// -1(because s3 < s1 )
+    }
 }
